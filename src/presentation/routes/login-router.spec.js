22e9309dec0c98812f2845bbc034c04baac047ab
Gabriel Campos <gabrielcmps55@gmail.com>
2023-10-20 21:18:50 -0300

    feat: Ensure LoginRouter returns 400 if no password is provided

--- src/presentation/routes/login-router.spec.js
+++ src/presentation/routes/login-router.spec.js
@@ -1,8 +1,11 @@
 class LoginRouter {
   route (httpRequest) {
-    const { email } = httpRequest.body
-    if (!email) {
-      return { statusCode: 400 }
+    const { email, password } = httpRequest.body
+
+    if (!email || !password) {
+      return {
+        statusCode: 400
+      }
     }
   }
 }
@@ -13,6 +16,17 @@ describe('Login Router', () => {
     const httpRequest = {
       body: {
         password: 'any_password'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+
+  test('Should return 400 if no password is provided', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        email: 'example@example.com'
       }
     }
     const httpResponse = sut.route(httpRequest)
