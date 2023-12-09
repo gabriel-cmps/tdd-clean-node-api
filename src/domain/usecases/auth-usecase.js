3760178b7aab5f29afb09f2c1b8c7d527ad728ab
Gabriel Campos <gabrielcmps55@gmail.com>
2023-12-08 21:04:28 -0300

    refactor: Remove unnecessary ifs

--- src/domain/usecases/auth-usecase.js
+++ src/domain/usecases/auth-usecase.js
@@ -1,4 +1,4 @@
-const { MissingParamError, InvalidParamError } = require('../../utils/errors')
+const { MissingParamError } = require('../../utils/errors')
 
 class AuthUseCase {
   constructor (loadUserByEmailRepository) {
@@ -13,13 +13,6 @@ class AuthUseCase {
       throw new MissingParamError('password')
     }
 
-    if (!this.loadUserByEmailRepository) {
-      throw new MissingParamError('loadUserByEmailRepository')
-    }
-
-    if (!this.loadUserByEmailRepository.load) {
-      throw new InvalidParamError('loadUserByEmailRepository')
-    }
     const user = await this.loadUserByEmailRepository.load(email)
 
     if (!user) return null
