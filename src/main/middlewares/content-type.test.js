56f456da835a17f38b7238467b098ed7bdf2f7e2
Gabriel Campos <gabrielcmps55@gmail.com>
2023-12-12 02:19:01 -0300

    feat: Ensure requests return content-type as json by default

---
+++ src/main/middlewares/content-type.test.js
@@ -0,0 +1,11 @@
+const request = require('supertest')
+const app = require('../config/app')
+
+describe('Content-type Middleware', () => {
+  test('Should return content type as default', async () => {
+    app.get('/test_content_type', (req, res) => {
+      res.send('')
+    })
+    await request(app).get('/test_content_type').expect('Content-Type', /json/)
+  })
+})
