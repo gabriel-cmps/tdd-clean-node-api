a52b2dcb9633463ec3320d9d651e6d821f1a2bab
Gabriel Campos <gabrielcmps55@gmail.com>
2023-12-13 00:37:51 -0300

    feat: Ensure requests return given content-type when forced

--- src/main/middlewares/content-type.test.js
+++ src/main/middlewares/content-type.test.js
@@ -2,10 +2,18 @@ const request = require('supertest')
 const app = require('../config/app')
 
 describe('Content-type Middleware', () => {
-  test('Should return content type as default', async () => {
+  test('Should return content-type as default', async () => {
     app.get('/test_content_type', (req, res) => {
       res.send('')
     })
     await request(app).get('/test_content_type').expect('Content-Type', /json/)
+  })
+
+  test('Should return xml content-type if forced', async () => {
+    app.get('/test_content_type_xml', (req, res) => {
+      res.type('xml')
+      res.send('')
+    })
+    await request(app).get('/test_content_type_xml').expect('Content-Type', /xml/)
   })
 })
